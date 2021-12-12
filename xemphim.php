e9de184c5b0806c4e238784923e33135e3c8b101
LamKeee <namnh14162000@gmail.com>
2021-12-12 19:44:58 +0700

    update database + update xemphim.php

--- xemphim.php
+++ xemphim.php
@@ -1,1 +1,127 @@
-Đây là trang xem phim chi tiết
+<html>
+<meta httlp-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="/css/style.css" type="text/css" media="all" />
+
+
+<head>
+
+</head>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "film_test";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+  if ($conn->connect_error) {
+  die("Connection failed:" . $conn->connect_error);
+}
+
+
+
+$sql_05="SELECT * FROM film where tenFilm='Brazil' ";
+$result_05=$conn->query($sql_05);
+
+?>
+
+
+
+<body>
+
+    <div id="header">
+        <div class="container">
+
+            <a class="logo" href="/index.php" title="Hnam.com">
+                <img src="/img/logo/logophim.png" style="width:200px;padding-top:2px;margin: 2px">
+        </div>
+        <div class="search-container relative">
+            <form action="javascript:MakeSearch();" class="form-search">
+                <input id="keyword" type="text" placeholder="Nhập tên phim, diễn viên ...">
+            </form>
+        </div>
+    </div>
+
+
+
+    <div id="main-menu">
+        <ul class="container">
+            <li><a class="active" href="/index.php">Trang chủ</a></li>
+            <li><a href="#">Phim lẻ </a></li>
+            <li><a href="#">Phim chiếu rạp</a></li>
+            <li><a href="#">Phim bộ</a></li>
+            <li><a href="#">Phim bộ</a></li>
+            <li><a href="#">Phim Mỹ</a></li>
+            <li><a href="#">Phim Englishsub</a></li>
+            <li><a href="#">Liên hệ quảng cáo</a></li>
+        </ul>
+    </div>
+
+
+
+
+    <div id="main-content-detail">
+        <div id="content-detail">
+            <div class="container" id="detail-page">
+                <div class="film-info">
+                    <?php 
+                        if ($result_05->num_rows > 0) {
+                          while ($row = $result_05->fetch_assoc()) {
+                        
+                        ?>
+                    <div id="avatar-film">
+                        <a href="#"><img src="<?= $row["avt"] ?>" alt="" /></a>
+                    </div>
+                    <div id="film-info-detail">
+                        Tên phim: <span class="movie_information"><?= $row["tenFilm"] ?></span>
+                        Năm sản xuất: <span class="movie_information"><?=$row["namSanXuat"]?></span>
+                        Đạo diễn: <span class="movie_information"><?= $row["daoDien"]?></span>
+                        Lượt xem: <span class="movie_information"><?=$row["luotXem"]?></span>
+                        Mô tả:<span class="movie_information"><?=$row["moTa"]?></span>
+
+
+
+                        <?php
+              }
+            }
+           ?>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+            <?php 
+// End of main content ( include film and film info)
+?>
+
+
+
+        </div>
+
+      <div> 
+
+
+    </div>
+
+
+
+
+
+</body>
+<footer>
+    <div id="contact">
+        <p>Author: HNAM Nguyen</p>
+        <p>SDT lien he : 012345678910JQK</p>
+        <p><a href="mailto:hege@example.com">namhoangIT2021@gmail.com</a></p>
+
+
+    </div>
+</footer>
+
+</html>
